33a5975c64b9b39959c02ba1f061313445f2cbb4
TomasDurica <tomas.durica.sk@gmail.com>
2024-01-19 15:26:34 +0100

    Fixed Result extensions parameter naming

--- src/Langutils.Core/Results/ResultExtensions.Logical.cs
+++ src/Langutils.Core/Results/ResultExtensions.Logical.cs
@@ -6,15 +6,15 @@ public static partial class ResultExtensions
     /// Returns the second Result if the first Result is a success, otherwise returns the first Result.
     /// </summary>
     /// <param name="self">The first Result.</param>
-    /// <param name="option">The second Result.</param>
+    /// <param name="result">The second Result.</param>
     /// <typeparam name="TIn">The type of the value in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the value in the second Result.</typeparam>
     /// <typeparam name="TError">The type of the error in the Results.</typeparam>
     /// <returns>The second Result if the first Result is a success, otherwise the first Result.</returns>
-    public static Result<TOut, TError> And<TIn, TOut, TError>(this Result<TIn, TError> self, Result<TOut, TError> option)
+    public static Result<TOut, TError> And<TIn, TOut, TError>(this Result<TIn, TError> self, Result<TOut, TError> result)
         => self switch
         {
-            { IsSuccess: true } => option,
+            { IsSuccess: true } => result,
             { Error: var error} => error!
         };
 
@@ -22,13 +22,13 @@ public static partial class ResultExtensions
     /// Asynchronously returns the second Result if the first Result is a success, otherwise returns the first Result.
     /// </summary>
     /// <param name="self">The Task of the first Result.</param>
-    /// <param name="option">The second Result.</param>
+    /// <param name="result">The second Result.</param>
     /// <typeparam name="TIn">The type of the value in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the value in the second Result.</typeparam>
     /// <typeparam name="TError">The type of the error in the Results.</typeparam>
     /// <returns>A Task of the second Result if the first Result is a success, otherwise the first Result.</returns>
-    public static async Task<Result<TOut, TError>> And<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Result<TOut, TError> option)
-        => (await self.ConfigureAwait(false)).And(option);
+    public static async Task<Result<TOut, TError>> And<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Result<TOut, TError> result)
+        => (await self.ConfigureAwait(false)).And(result);
 
     /// <summary>
     /// Returns the result of the provided function if the first Result is a success, otherwise returns the first Result.
@@ -55,8 +55,8 @@ public static partial class ResultExtensions
     /// <typeparam name="TOut">The type of the value in the second Result.</typeparam>
     /// <typeparam name="TError">The type of the error in the Results.</typeparam>
     /// <returns>A Task of the result of the provided function if the first Result is a success, otherwise the first Result.</returns>
-    public static async Task<Result<TOut, TError>> AndThen<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Func<TIn, Result<TOut, TError>> optionProvider)
-        => (await self.ConfigureAwait(false)).AndThen(optionProvider);
+    public static async Task<Result<TOut, TError>> AndThen<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Func<TIn, Result<TOut, TError>> resultProvider)
+        => (await self.ConfigureAwait(false)).AndThen(resultProvider);
 
     /// <summary>
     /// Returns the result of the provided asynchronous function if the first Result is a success, otherwise returns the first Result.
@@ -67,10 +67,10 @@ public static partial class ResultExtensions
     /// <typeparam name="TOut">The type of the value in the second Result.</typeparam>
     /// <typeparam name="TError">The type of the error in the Results.</typeparam>
     /// <returns>A Task of the result of the provided asynchronous function if the first Result is a success, otherwise the first Result.</returns>
-    public static async Task<Result<TOut, TError>> AndThenAsync<TIn, TOut, TError>(this Result<TIn, TError> self, Func<TIn, Task<Result<TOut, TError>>> optionProvider)
+    public static async Task<Result<TOut, TError>> AndThenAsync<TIn, TOut, TError>(this Result<TIn, TError> self, Func<TIn, Task<Result<TOut, TError>>> resultProvider)
         => self switch
         {
-            { IsSuccess: true, Value: var value} => await optionProvider(value).ConfigureAwait(false),
+            { IsSuccess: true, Value: var value} => await resultProvider(value).ConfigureAwait(false),
             { Error: var error } => error!
         };
 
@@ -83,36 +83,36 @@ public static partial class ResultExtensions
     /// <typeparam name="TOut">The type of the value in the second Result.</typeparam>
     /// <typeparam name="TError">The type of the error in the Results.</typeparam>
     /// <returns>A Task of the result of the provided asynchronous function if the first Result is a success, otherwise the first Result.</returns>
-    public static async Task<Result<TOut, TError>> AndThenAsync<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Func<TIn, Task<Result<TOut, TError>>> optionProvider)
-        => await (await self.ConfigureAwait(false)).AndThenAsync(optionProvider).ConfigureAwait(false);
+    public static async Task<Result<TOut, TError>> AndThenAsync<TIn, TOut, TError>(this Task<Result<TIn, TError>> self, Func<TIn, Task<Result<TOut, TError>>> resultProvider)
+        => await (await self.ConfigureAwait(false)).AndThenAsync(resultProvider).ConfigureAwait(false);
 
     /// <summary>
     /// Returns the first Result if it is a success, otherwise returns the second Result.
     /// </summary>
     /// <param name="self">The first Result.</param>
-    /// <param name="option">The second Result.</param>
+    /// <param name="result">The second Result.</param>
     /// <typeparam name="TValue">The type of the value in the Results.</typeparam>
     /// <typeparam name="TIn">The type of the error in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the error in the second Result.</typeparam>
     /// <returns>The first Result if it is a success, otherwise the second Result.</returns>
-    public static Result<TValue, TOut> Or<TValue, TIn, TOut>(this Result<TValue, TIn> self, Result<TValue, TOut> option)
+    public static Result<TValue, TOut> Or<TValue, TIn, TOut>(this Result<TValue, TIn> self, Result<TValue, TOut> result)
         => self switch
         {
             { IsSuccess: true, Value: var value } => value,
-            _ => option
+            _ => result
         };
 
     /// <summary>
     /// Asynchronously returns the first Result if it is a success, otherwise returns the second Result.
     /// </summary>
     /// <param name="self">The Task of the first Result.</param>
-    /// <param name="option">The second Result.</param>
+    /// <param name="result">The second Result.</param>
     /// <typeparam name="TValue">The type of the value in the Results.</typeparam>
     /// <typeparam name="TIn">The type of the error in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the error in the second Result.</typeparam>
     /// <returns>A Task of the first Result if it is a success, otherwise the second Result.</returns>
-    public static async Task<Result<TValue, TOut>> Or<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Result<TValue, TOut> option)
-        => (await self.ConfigureAwait(false)).Or(option);
+    public static async Task<Result<TValue, TOut>> Or<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Result<TValue, TOut> result)
+        => (await self.ConfigureAwait(false)).Or(result);
 
     /// <summary>
     /// Returns the first Result if it is a success, otherwise returns the result of the provided function.
@@ -139,8 +139,8 @@ public static partial class ResultExtensions
     /// <typeparam name="TIn">The type of the error in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the error in the second Result.</typeparam>
     /// <returns>A Task of the first Result if it is a success, otherwise the result of the provided function.</returns>
-    public static async Task<Result<TValue, TOut>> OrElse<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Func<TIn?, Result<TValue, TOut>> optionProvider)
-        => (await self.ConfigureAwait(false)).OrElse(optionProvider);
+    public static async Task<Result<TValue, TOut>> OrElse<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Func<TIn?, Result<TValue, TOut>> resultProvider)
+        => (await self.ConfigureAwait(false)).OrElse(resultProvider);
 
     /// <summary>
     /// Returns the first Result if it is a success, otherwise returns the result of the provided asynchronous function.
@@ -151,11 +151,11 @@ public static partial class ResultExtensions
     /// <typeparam name="TIn">The type of the error in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the error in the second Result.</typeparam>
     /// <returns>A Task of the first Result if it is a success, otherwise the result of the provided asynchronous function.</returns>
-    public static async Task<Result<TValue, TOut>> OrElseAsync<TValue, TIn, TOut>(this Result<TValue, TIn> self, Func<TIn?, Task<Result<TValue, TOut>>> optionProvider)
+    public static async Task<Result<TValue, TOut>> OrElseAsync<TValue, TIn, TOut>(this Result<TValue, TIn> self, Func<TIn?, Task<Result<TValue, TOut>>> resultProvider)
         => self switch
         {
             { IsSuccess: true, Value: var value } => value,
-            { Error: var error } => await optionProvider(error).ConfigureAwait(false)
+            { Error: var error } => await resultProvider(error).ConfigureAwait(false)
         };
 
     /// <summary>
@@ -167,6 +167,6 @@ public static partial class ResultExtensions
     /// <typeparam name="TIn">The type of the error in the first Result.</typeparam>
     /// <typeparam name="TOut">The type of the error in the second Result.</typeparam>
     /// <returns>A Task of the first Result if it is a success, otherwise the result of the provided asynchronous function.</returns>
-    public static async Task<Result<TValue, TOut>> OrElseAsync<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Func<TIn?, Task<Result<TValue, TOut>>> optionProvider)
-        => await (await self.ConfigureAwait(false)).OrElseAsync(optionProvider).ConfigureAwait(false);
+    public static async Task<Result<TValue, TOut>> OrElseAsync<TValue, TIn, TOut>(this Task<Result<TValue, TIn>> self, Func<TIn?, Task<Result<TValue, TOut>>> resultProvider)
+        => await (await self.ConfigureAwait(false)).OrElseAsync(resultProvider).ConfigureAwait(false);
 }
