6a61a14327272fe8f0178ff12ae127878f7fdcb3
TomasDurica <tomas.durica.sk@gmail.com>
2024-01-29 15:38:11 +0100

    Added an ability to create Unit through empty collection initializer

--- src/Langutils.Core/Unit.cs
+++ src/Langutils.Core/Unit.cs
@@ -1,12 +1,26 @@
-﻿namespace Langutils.Core;
+﻿using System.Collections;
+
+namespace Langutils.Core;
 
 /// <summary>
 /// This type is used to represent a unit value (or no value).
 /// </summary>
-public readonly record struct Unit
+public readonly record struct Unit : IEnumerable<Unit>
 {
     /// <summary>
     /// The only instance of the Unit type.
     /// </summary>
     public static readonly Unit Instance = new();
+
+    /// <summary>
+    /// Returns an empty enumerable of Unit.
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator<Unit> GetEnumerator()
+    {
+        yield break;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
 }
