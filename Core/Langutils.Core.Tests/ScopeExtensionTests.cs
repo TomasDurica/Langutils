58b480ef21aa7e96e881599841b830dd3a1586e7
TomasDurica <tomas.durica.sk@gmail.com>
2024-12-13 21:01:57 +0100

    Added scope functions `Let` and `Also` inspired by Kotlin

---
+++ Core/Langutils.Core.Tests/ScopeExtensionTests.cs
@@ -0,0 +1,169 @@
+﻿using NSubstitute;
+
+namespace Langutils.Core.Tests;
+
+public class ScopeExtensionTests
+{
+    private static readonly object Value = new();
+    private static readonly object OtherValue = new();
+    private static readonly Task<object> ValueTask = Task.FromResult(Value);
+    private static readonly Task<object> OtherValueTask = Task.FromResult(OtherValue);
+
+    [Fact]
+    public void Also_ReturnsOriginalValue()
+    {
+        var result = Value.Also(v => { });
+
+        Assert.Same(Value, result);
+    }
+
+    [Fact]
+    public async Task Also_WhenValueIsTask_ReturnsOriginalValue()
+    {
+        var result = await ValueTask.Also(v => { });
+
+        Assert.Same(Value, result);
+    }
+
+    [Fact]
+    public async Task AlsoAsync_ReturnsOriginalValue()
+    {
+        var result = await Value.AlsoAsync(v => Task.CompletedTask);
+
+        Assert.Same(Value, result);
+    }
+
+    [Fact]
+    public async Task AlsoAsync_WhenValueIsTask_ReturnsOriginalValue()
+    {
+        var result = await ValueTask.AlsoAsync(v => Task.CompletedTask);
+
+        Assert.Same(Value, result);
+    }
+
+    [Fact]
+    public void Also_CallsAction()
+    {
+        var action = Substitute.For<Action<object>>();
+
+        Value.Also(action);
+
+        var call = Assert.Single(action.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task Also_WhenValueIsTask_CallsAction()
+    {
+        var action = Substitute.For<Action<object>>();
+
+        await ValueTask.Also(action);
+
+        var call = Assert.Single(action.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task AlsoAsync_CallsAction()
+    {
+        var action = Substitute.For<Func<object, Task>>();
+        action.Invoke(Arg.Any<object>()).Returns(Task.CompletedTask);
+
+        await Value.AlsoAsync(action);
+
+        var call = Assert.Single(action.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task AlsoAsync_WhenValueIsTask_CallsAction()
+    {
+        var action = Substitute.For<Func<object, Task>>();
+        action.Invoke(Arg.Any<object>()).Returns(Task.CompletedTask);
+
+        await ValueTask.AlsoAsync(action);
+
+        var call = Assert.Single(action.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public void Let_ReturnsProjectedValue()
+    {
+        var result = Value.Let(v => OtherValue);
+
+        Assert.Same(OtherValue, result);
+    }
+
+    [Fact]
+    public async Task Let_WhenValueIsTask_ReturnsProjectedValue()
+    {
+        var result = await ValueTask.Let(v => OtherValue);
+
+        Assert.Same(OtherValue, result);
+    }
+
+    [Fact]
+    public async Task LetAsync_ReturnsProjectedValue()
+    {
+        var result = await Value.LetAsync(v => OtherValueTask);
+
+        Assert.Same(OtherValue, result);
+    }
+
+    [Fact]
+    public async Task LetAsync_WhenValueIsTask_ReturnsProjectedValue()
+    {
+        var result = await ValueTask.LetAsync(v => OtherValueTask);
+
+        Assert.Same(OtherValue, result);
+    }
+
+    [Fact]
+    public void Let_CallsProjection()
+    {
+        var projection = Substitute.For<Func<object, object>>();
+        projection.Invoke(Arg.Any<object>()).Returns(OtherValue);
+
+        Value.Let(projection);
+
+        var call = Assert.Single(projection.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task Let_WhenValueIsTask_CallsProjection()
+    {
+        var projection = Substitute.For<Func<object, object>>();
+        projection.Invoke(Arg.Any<object>()).Returns(OtherValue);
+
+        await ValueTask.Let(projection);
+
+        var call = Assert.Single(projection.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task LetAsync_CallsProjection()
+    {
+        var projection = Substitute.For<Func<object, Task<object>>>();
+        projection.Invoke(Arg.Any<object>()).Returns(OtherValueTask);
+
+        await Value.Let(projection);
+
+        var call = Assert.Single(projection.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+
+    [Fact]
+    public async Task LetAsync_WhenValueIsTask_CallsProjection()
+    {
+        var projection = Substitute.For<Func<object, Task<object>>>();
+        projection.Invoke(Arg.Any<object>()).Returns(OtherValueTask);
+
+        await ValueTask.Let(projection);
+
+        var call = Assert.Single(projection.ReceivedCalls());
+        Assert.Same(Value, call.GetArguments().Single());
+    }
+}
---
+++ Core/Langutils.Core/ScopeExtensions.cs
@@ -0,0 +1,97 @@
+﻿namespace Langutils.Core;
+
+/// <summary>
+/// Extension methods for inline value inspection and manipulation.
+/// </summary>
+public static class ScopeExtensions
+{
+    /// <summary>
+    /// This method is used to execute an action on a value while returning the value itself.
+    /// </summary>
+    /// <param name="self">The value that the action is executed on</param>
+    /// <param name="action">The action that should be executed on the value</param>
+    /// <typeparam name="T">The value type</typeparam>
+    /// <returns>The value</returns>
+    public static T Also<T>(this T self, Action<T> action)
+    {
+        action(self);
+        return self;
+    }
+
+    /// <summary>
+    /// This method is used to execute an action on a value task while returning the value itself.
+    /// </summary>
+    /// <param name="self">The value task that the action is executed on</param>
+    /// <param name="action">The action that should be executed on the value</param>
+    /// <typeparam name="T">The value type</typeparam>
+    /// <returns>The value</returns>
+    public static async Task<T> Also<T>(this Task<T> self, Action<T> action)
+        => (await self.ConfigureAwait(false)).Also(action);
+
+    /// <summary>
+    /// This method is used to execute an asynchronous action on a value while returning the value itself.
+    /// </summary>
+    /// <param name="self">The value that the asynchronous action is executed on</param>
+    /// <param name="action">The asynchronous action that should be executed on the value</param>
+    /// <typeparam name="T">The value type</typeparam>
+    /// <returns>The value</returns>
+    public static async Task<T> AlsoAsync<T>(this T self, Func<T, Task> action)
+    {
+        await action(self).ConfigureAwait(false);
+        return self;
+    }
+
+    /// <summary>
+    /// This method is used to execute an asynchronous action on a value task while returning the value itself.
+    /// </summary>
+    /// <param name="self">The value task that the asynchronous action is executed on</param>
+    /// <param name="action">The asynchronous action that should be executed on the value</param>
+    /// <typeparam name="T">The value type</typeparam>
+    /// <returns>The value</returns>
+    public static async Task<T> AlsoAsync<T>(this Task<T> self, Func<T, Task> action)
+        => await (await self.ConfigureAwait(false)).AlsoAsync(action).ConfigureAwait(false);
+
+    /// <summary>
+    /// This method is used to modify a value inline by using a function.
+    /// </summary>
+    /// <param name="value">The value that the function is executed on</param>
+    /// <param name="func">The function the value is transformed by</param>
+    /// <typeparam name="TIn">The value type</typeparam>
+    /// <typeparam name="TOut">The function result type</typeparam>
+    /// <returns>The result of the function</returns>
+    public static TOut Let<TIn, TOut>(this TIn value, Func<TIn, TOut> func)
+        => func(value);
+
+    /// <summary>
+    /// This method is used to modify a value task inline by using a function.
+    /// </summary>
+    /// <param name="value">The value task that the function is executed on</param>
+    /// <param name="projection">The function the value is transformed by</param>
+    /// <typeparam name="TIn">The value type</typeparam>
+    /// <typeparam name="TOut">The function result type</typeparam>
+    /// <returns>The result of the function</returns>
+    public static async Task<TOut> Let<TIn, TOut>(this Task<TIn> value, Func<TIn, TOut> projection)
+        => projection(await value.ConfigureAwait(false));
+
+    /// <summary>
+    /// This method is used to modify a value inline by using an asynchronous function.
+    /// </summary>
+    /// <param name="value">The value that the asynchronous function is executed on</param>
+    /// <param name="projection">The asynchronous function the value is transformed by</param>
+    /// <typeparam name="TIn">The value type</typeparam>
+    /// <typeparam name="TOut">The function result type</typeparam>
+    /// <returns>The result of the function</returns>
+    public static async Task<TOut> LetAsync<TIn, TOut>(this TIn value, Func<TIn, Task<TOut>> projection)
+        => await projection(value).ConfigureAwait(false);
+
+    /// <summary>
+    /// This method is used to modify a value task inline by using an asynchronous function.
+    /// </summary>
+    /// <param name="value">The value task that the asynchronous function is executed on</param>
+    /// <param name="projection">The asynchronous function the value is transformed by</param>
+    /// <typeparam name="TIn">The value type</typeparam>
+    /// <typeparam name="TOut">The function result type</typeparam>
+    /// <returns>The result of the function</returns>
+    public static async Task<TOut> LetAsync<TIn, TOut>(this Task<TIn> value, Func<TIn, Task<TOut>> projection)
+        => await (await value.ConfigureAwait(false)).LetAsync(projection).ConfigureAwait(false);
+}
